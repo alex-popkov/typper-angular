71bc8b979667ca8e50a5959f50571eb0b03db956
alex-popkov <popkov.aleksander@gmail.com>
2016-08-21 19:10:10 +0300

    html tpl was added

--- app/typper.singlePost/singlePost.component.js
+++ app/typper.singlePost/singlePost.component.js
@@ -3,15 +3,47 @@
 // Register `typper.singlePost` component, along with its associated controller and template
 angular
 
-    .module('typper.singlePost')
+    .module( 'typper.singlePost' )
 
     .component( 'singlePostComponent', {
 
         templateUrl: 'app/typper.singlePost/templates/singlePost.template.html',
-        controller: [ '$scope', '$http', '$window',
+        controller: [ '$scope', '$routeParams', '$http', '$window', 'GoldenratioMath', 'SINGLE_POST_CONSTANTS',
 
-            function postsCtrl( $scope ) {
+            function singlePostCtrl( $scope, $routeParams, $http, $window, GoldenratioMath, SINGLE_POST_CONSTANTS ){
 
+                this.id = $routeParams.id;
+                $scope.singlePostConstants = SINGLE_POST_CONSTANTS;
+                var self = this;
+
+                $scope.viewportHeight = $window.innerHeight -
+
+                    angular.element( document.querySelectorAll( '.app-toolbar' ) )[ 0 ].clientHeight;
+
+                $scope.viewportWidth = angular.element( document.querySelectorAll( '.view-content' ) )[ 0 ].clientWidth;
+
+                var destroy = angular.element( window ).bind( 'resize', function( ){
+
+                    $scope.$apply( function( ){
+
+                        $scope.viewportHeight = $window.innerHeight -
+
+                            angular.element( document.querySelectorAll( '.app-toolbar' ) )[ 0 ].clientHeight;
+
+                        $scope.viewportWidth = angular.element( document.querySelectorAll( '.view-content' ) )[ 0 ].clientWidth;
+                        $scope.postsRowWidth = GoldenratioMath.getGoldenSize(
+
+                            self.calculateViewportSize( ).width, self.calculateViewportSize( ).height ).width;
+                    } );
+                });
+
+                this.calculateViewportSize = function( ){
+
+                    return GoldenratioMath.getGoldenSize( $scope.viewportWidth, $scope.viewportHeight );
+                };
+                $scope.postsRowWidth = GoldenratioMath.getGoldenSize(
+
+                    this.calculateViewportSize( ).width, this.calculateViewportSize( ).height ).width;
             }
         ]
     });
